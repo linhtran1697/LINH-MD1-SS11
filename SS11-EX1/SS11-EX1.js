290e56a70d5dab4b56c632ede71c7efc19ae75d2
linhtran1697 <linhtran.1697.lt@gmail.com>
2026-05-25 20:23:06 +0900

    SS11-EX1

---
+++ SS11-EX1/SS11-EX1.js
@@ -0,0 +1,8 @@
+
+let year = Number(prompt('Hãy nhập năm bạn muốn kiểm tra: '));
+if (year % 4 === 0 && year % 100 !== 0) {
+  alert(`${year} là năm nhuận`);
+} else {
+  alert(`${year} không phải năm nhuận`);
+}
+
